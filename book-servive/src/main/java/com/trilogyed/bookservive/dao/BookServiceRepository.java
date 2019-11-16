170703baabd8c198817154730d06be1d1e3a9fab
jyotichavan2019 <chavan.jyoti.2011@gmail.com>
2019-11-15 21:59:41 -0600

    adding properties

--- book-servive/src/main/java/com/trilogyed/bookservive/dao/BookServiceRepository.java
+++ book-servive/src/main/java/com/trilogyed/bookservive/dao/BookServiceRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface BookServiceRepository extends JpaRepository<Book, Integer> {
-
+    Book save(Book book);
 }
